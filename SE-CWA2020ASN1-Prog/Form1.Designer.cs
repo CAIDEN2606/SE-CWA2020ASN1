a0d3f79eda6449110fee45012447721d2a1abf39
Caiden Farmer <409854@edu.cwa.ac.uk>
2021-10-14 20:52:09 +0100

    Added a "Caiden's TEST" label

--- SE-CWA2020ASN1-Prog/Form1.Designer.cs
+++ SE-CWA2020ASN1-Prog/Form1.Designer.cs
@@ -31,24 +31,38 @@ namespace SE_CWA2020ASN1_Prog
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbl_Test = new System.Windows.Forms.Label();
+            this.lbl_Caiden = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbl_Test
             // 
             this.lbl_Test.AutoSize = true;
             this.lbl_Test.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Test.Location = new System.Drawing.Point(12, 9);
+            this.lbl_Test.Location = new System.Drawing.Point(16, 14);
+            this.lbl_Test.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_Test.Name = "lbl_Test";
-            this.lbl_Test.Size = new System.Drawing.Size(784, 175);
+            this.lbl_Test.Size = new System.Drawing.Size(1140, 224);
             this.lbl_Test.TabIndex = 0;
             this.lbl_Test.Text = resources.GetString("lbl_Test.Text");
             // 
+            // lbl_Caiden
+            // 
+            this.lbl_Caiden.AutoSize = true;
+            this.lbl_Caiden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Caiden.Location = new System.Drawing.Point(22, 615);
+            this.lbl_Caiden.Name = "lbl_Caiden";
+            this.lbl_Caiden.Size = new System.Drawing.Size(204, 32);
+            this.lbl_Caiden.TabIndex = 1;
+            this.lbl_Caiden.Text = "Caiden\'s TEST";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1100, 675);
+            this.Controls.Add(this.lbl_Caiden);
             this.Controls.Add(this.lbl_Test);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -59,6 +73,7 @@ namespace SE_CWA2020ASN1_Prog
         #endregion
 
         private System.Windows.Forms.Label lbl_Test;
+        private System.Windows.Forms.Label lbl_Caiden;
     }
 }
 
