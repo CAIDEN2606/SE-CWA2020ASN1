85438c228645b96bad8f681944fa13f8404ad78d
Caiden Farmer <409854@edu.cwa.ac.uk>
2021-10-14 18:16:00 +0100

    Added a Reminder Message

--- SE-CWA2020ASN1-Prog/Form1.Designer.cs
+++ SE-CWA2020ASN1-Prog/Form1.Designer.cs
@@ -29,13 +29,36 @@ namespace SE_CWA2020ASN1_Prog
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_Test = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Test
+            // 
+            this.lbl_Test.AutoSize = true;
+            this.lbl_Test.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Test.Location = new System.Drawing.Point(12, 9);
+            this.lbl_Test.Name = "lbl_Test";
+            this.lbl_Test.Size = new System.Drawing.Size(784, 25);
+            this.lbl_Test.TabIndex = 0;
+            this.lbl_Test.Text = "Remember to perform a fetch, and a pull, BEFORE, commiting any of your own change" +
+    "s :)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_Test);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_Test;
     }
 }
 
