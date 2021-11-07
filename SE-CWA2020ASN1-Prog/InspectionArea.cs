9580ebfddd097ddb33a6e284c420f0077d0e69b0
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2021-11-07 10:52:06 +0000

    added to classes to store data in lists prior to add sqlite

--- SE-CWA2020ASN1-Prog/InspectionArea.cs
+++ SE-CWA2020ASN1-Prog/InspectionArea.cs
@@ -8,11 +8,29 @@ namespace SE_CWA2020ASN1_Prog
 {
     class InspectionArea
     {
-        private string m_workareaName;
-        public string Work_Area { get { return m_workareaName; } set { m_workareaName = value; } }
+        private String m_workarea;
+        private String m_inspectionSummary;
 
-        private string m_inspectionSummary;
-        public string Inspection_Summary { get { return m_inspectionSummary; } set { m_inspectionSummary = value; } }
+        private List<Intervention> m_interventionObjs;
+
+        public InspectionArea(String workArea, String iSummary)
+        {
+            m_workarea = workArea;
+            m_inspectionSummary = iSummary;
+            m_interventionObjs = new List<Intervention>();
+        }
+        /// <summary>
+        /// multiple intervention objects added to single work area list
+        /// </summary>
+        public void addIntervention(Intervention i)
+        {
+            m_interventionObjs.Add(i);
+        }
+        public String Work_Area { get { return m_workarea; } set { m_workarea = value; } }
+
+
+        public String Inspection_Summary { get { return m_inspectionSummary; } set { m_inspectionSummary = value; } }
 
     }
+}
 }
