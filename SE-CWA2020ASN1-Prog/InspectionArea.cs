331845b8c35a9d6410d5f4d3c67052eeaa8c907e
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2021-11-09 11:46:03 +0000

    changed String back to string in line with coding standards, my bad

--- SE-CWA2020ASN1-Prog/InspectionArea.cs
+++ SE-CWA2020ASN1-Prog/InspectionArea.cs
@@ -8,17 +8,17 @@ namespace SE_CWA2020ASN1_Prog
 {
     class InspectionArea
     {
-        private String m_workarea;
-        private String m_inspectionSummary;
+        private string m_workarea;
+        private string m_inspectionSummary;
 
         private List<Intervention> m_interventionObjs;
 
-        public InspectionArea(String workArea, String iSummary)
-        {
-            m_workarea = workArea;
-            m_inspectionSummary = iSummary;
-            m_interventionObjs = new List<Intervention>();
-        }
+        //public InspectionArea(string workArea, string iSummary)
+        //{
+        //    m_workarea = workArea;
+        //    m_inspectionSummary = iSummary;
+        //    m_interventionObjs = new List<Intervention>();
+        //}
         /// <summary>
         /// multiple intervention objects added to single work area list
         /// </summary>
@@ -26,10 +26,10 @@ namespace SE_CWA2020ASN1_Prog
         {
             m_interventionObjs.Add(i);
         }
-        public String Work_Area { get { return m_workarea; } set { m_workarea = value; } }
+        public string Work_Area { get { return m_workarea; } set { m_workarea = value; } }
 
 
-        public String Inspection_Summary { get { return m_inspectionSummary; } set { m_inspectionSummary = value; } }
+        public string Inspection_Summary { get { return m_inspectionSummary; } set { m_inspectionSummary = value; } }
 
     }
 }
