d627372028f9ea57d83a93d55e560c68fb991187
Lawrence Abu-Jaber <71214072+labujaber1@users.noreply.github.com>
2021-12-11 17:20:36 +0000

    fixed logo image to stay put.

--- SE-CWA2020ASN1-Prog/SafetyInspection1.Designer.cs
+++ SE-CWA2020ASN1-Prog/SafetyInspection1.Designer.cs
@@ -45,88 +45,89 @@ namespace SE_CWA2020ASN1_Prog
             this.txt_jobDescription = new System.Windows.Forms.TextBox();
             this.cmb_EnterType = new System.Windows.Forms.ComboBox();
             this.txt_supervisor = new System.Windows.Forms.TextBox();
-            this.pic_logo = new System.Windows.Forms.PictureBox();
             this.btn_about = new System.Windows.Forms.Button();
             this.btn_pdf = new System.Windows.Forms.Button();
+            this.pic_logo = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pic_logo)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(194, 141);
+            this.label1.Location = new System.Drawing.Point(218, 176);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(152, 17);
+            this.label1.Size = new System.Drawing.Size(174, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Safety Inspection Audit";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 258);
+            this.label2.Location = new System.Drawing.Point(40, 322);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.Size = new System.Drawing.Size(103, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Inspection by";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 180);
+            this.label3.Location = new System.Drawing.Point(40, 225);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.Size = new System.Drawing.Size(77, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Enter site";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 214);
+            this.label4.Location = new System.Drawing.Point(40, 268);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.Size = new System.Drawing.Size(0, 20);
             this.label4.TabIndex = 3;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(36, 222);
+            this.label5.Location = new System.Drawing.Point(40, 278);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(74, 17);
+            this.label5.Size = new System.Drawing.Size(84, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Enter date";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(37, 343);
+            this.label6.Location = new System.Drawing.Point(42, 429);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(138, 17);
+            this.label6.Size = new System.Drawing.Size(154, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "Enter job description";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(37, 300);
+            this.label7.Location = new System.Drawing.Point(42, 375);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(73, 17);
+            this.label7.Size = new System.Drawing.Size(82, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Enter type";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(36, 382);
+            this.label8.Location = new System.Drawing.Point(40, 478);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(112, 17);
+            this.label8.Size = new System.Drawing.Size(124, 20);
             this.label8.TabIndex = 7;
             this.label8.Text = "Enter supervisor";
             // 
             // btn_Enter
             // 
-            this.btn_Enter.Location = new System.Drawing.Point(29, 543);
+            this.btn_Enter.Location = new System.Drawing.Point(33, 603);
+            this.btn_Enter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Enter.Name = "btn_Enter";
-            this.btn_Enter.Size = new System.Drawing.Size(136, 34);
+            this.btn_Enter.Size = new System.Drawing.Size(153, 42);
             this.btn_Enter.TabIndex = 11;
             this.btn_Enter.Text = "Add intervention";
             this.btn_Enter.UseVisualStyleBackColor = true;
@@ -134,9 +135,10 @@ namespace SE_CWA2020ASN1_Prog
             // 
             // btn_exit
             // 
-            this.btn_exit.Location = new System.Drawing.Point(243, 543);
+            this.btn_exit.Location = new System.Drawing.Point(268, 603);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_exit.Name = "btn_exit";
-            this.btn_exit.Size = new System.Drawing.Size(152, 34);
+            this.btn_exit.Size = new System.Drawing.Size(171, 42);
             this.btn_exit.TabIndex = 13;
             this.btn_exit.Text = "Exit";
             this.btn_exit.UseVisualStyleBackColor = true;
@@ -146,68 +148,62 @@ namespace SE_CWA2020ASN1_Prog
             // 
             this.cmb_EnterSite.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmb_EnterSite.FormattingEnabled = true;
-            this.cmb_EnterSite.Location = new System.Drawing.Point(180, 172);
+            this.cmb_EnterSite.Location = new System.Drawing.Point(202, 215);
+            this.cmb_EnterSite.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmb_EnterSite.Name = "cmb_EnterSite";
-            this.cmb_EnterSite.Size = new System.Drawing.Size(198, 24);
+            this.cmb_EnterSite.Size = new System.Drawing.Size(222, 28);
             this.cmb_EnterSite.TabIndex = 5;
             // 
             // txt_inspectorName
             // 
-            this.txt_inspectorName.Location = new System.Drawing.Point(182, 258);
+            this.txt_inspectorName.Location = new System.Drawing.Point(205, 322);
+            this.txt_inspectorName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_inspectorName.MaxLength = 20;
             this.txt_inspectorName.Name = "txt_inspectorName";
-            this.txt_inspectorName.Size = new System.Drawing.Size(198, 22);
+            this.txt_inspectorName.Size = new System.Drawing.Size(222, 26);
             this.txt_inspectorName.TabIndex = 7;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(182, 222);
+            this.dateTimePicker1.Location = new System.Drawing.Point(205, 278);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(198, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(222, 26);
             this.dateTimePicker1.TabIndex = 6;
             // 
             // txt_jobDescription
             // 
-            this.txt_jobDescription.Location = new System.Drawing.Point(182, 338);
+            this.txt_jobDescription.Location = new System.Drawing.Point(205, 422);
+            this.txt_jobDescription.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_jobDescription.MaxLength = 20;
             this.txt_jobDescription.Name = "txt_jobDescription";
-            this.txt_jobDescription.Size = new System.Drawing.Size(198, 22);
+            this.txt_jobDescription.Size = new System.Drawing.Size(222, 26);
             this.txt_jobDescription.TabIndex = 9;
             // 
             // cmb_EnterType
             // 
             this.cmb_EnterType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmb_EnterType.FormattingEnabled = true;
-            this.cmb_EnterType.Location = new System.Drawing.Point(182, 300);
+            this.cmb_EnterType.Location = new System.Drawing.Point(205, 375);
+            this.cmb_EnterType.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmb_EnterType.Name = "cmb_EnterType";
-            this.cmb_EnterType.Size = new System.Drawing.Size(198, 24);
+            this.cmb_EnterType.Size = new System.Drawing.Size(222, 28);
             this.cmb_EnterType.TabIndex = 8;
             // 
             // txt_supervisor
             // 
-            this.txt_supervisor.Location = new System.Drawing.Point(182, 378);
+            this.txt_supervisor.Location = new System.Drawing.Point(205, 472);
+            this.txt_supervisor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_supervisor.Name = "txt_supervisor";
-            this.txt_supervisor.Size = new System.Drawing.Size(198, 22);
+            this.txt_supervisor.Size = new System.Drawing.Size(222, 26);
             this.txt_supervisor.TabIndex = 10;
-            // 
-            // pic_logo
-            // 
-            this.pic_logo.ImageLocation = "main.musk-logo.jpg";
-            this.pic_logo.InitialImage = global::SE_CWA2020ASN1_Prog.Properties.Resources.musk_logo;
-            this.pic_logo.Location = new System.Drawing.Point(29, 26);
-            this.pic_logo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pic_logo.Name = "pic_logo";
-            this.pic_logo.Size = new System.Drawing.Size(190, 89);
-            this.pic_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pic_logo.TabIndex = 38;
-            this.pic_logo.TabStop = false;
             // 
             // btn_about
             // 
-            this.btn_about.Location = new System.Drawing.Point(346, 26);
+            this.btn_about.Location = new System.Drawing.Point(389, 32);
             this.btn_about.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_about.Name = "btn_about";
-            this.btn_about.Size = new System.Drawing.Size(67, 25);
+            this.btn_about.Size = new System.Drawing.Size(75, 31);
             this.btn_about.TabIndex = 39;
             this.btn_about.Text = "About";
             this.btn_about.UseVisualStyleBackColor = true;
@@ -215,21 +211,35 @@ namespace SE_CWA2020ASN1_Prog
             // 
             // btn_pdf
             // 
-            this.btn_pdf.Location = new System.Drawing.Point(166, 462);
+            this.btn_pdf.Location = new System.Drawing.Point(178, 536);
             this.btn_pdf.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_pdf.Name = "btn_pdf";
-            this.btn_pdf.Size = new System.Drawing.Size(88, 34);
+            this.btn_pdf.Size = new System.Drawing.Size(99, 42);
             this.btn_pdf.TabIndex = 12;
             this.btn_pdf.Text = "PDF";
             this.btn_pdf.UseVisualStyleBackColor = true;
             this.btn_pdf.Click += new System.EventHandler(this.btn_pdf_Click);
             // 
+            // pic_logo
+            // 
+            this.pic_logo.ErrorImage = global::SE_CWA2020ASN1_Prog.Properties.Resources.musk_logo;
+            this.pic_logo.Image = global::SE_CWA2020ASN1_Prog.Properties.Resources.musk_logo;
+            this.pic_logo.ImageLocation = "";
+            this.pic_logo.InitialImage = global::SE_CWA2020ASN1_Prog.Properties.Resources.musk_logo;
+            this.pic_logo.Location = new System.Drawing.Point(33, 32);
+            this.pic_logo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pic_logo.Name = "pic_logo";
+            this.pic_logo.Size = new System.Drawing.Size(214, 111);
+            this.pic_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_logo.TabIndex = 38;
+            this.pic_logo.TabStop = false;
+            // 
             // SafetyInspection1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(436, 641);
+            this.ClientSize = new System.Drawing.Size(490, 801);
             this.Controls.Add(this.btn_pdf);
             this.Controls.Add(this.btn_about);
             this.Controls.Add(this.pic_logo);
@@ -249,6 +259,7 @@ namespace SE_CWA2020ASN1_Prog
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "SafetyInspection1";
             this.Text = "SafetyInspection";
             ((System.ComponentModel.ISupportInitialize)(this.pic_logo)).EndInit();
